f9c6c84af721f16027a81afe3b4cf8246086859d
zhangminglei <zml13856086071@163.com>
2016-08-31 10:34:52 +0800

    refine code

--- src/main/java/BasicJava/Fenjie.java
+++ src/main/java/BasicJava/Fenjie.java
@@ -1,6 +1,7 @@
 package BasicJava;
 
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by zhangminglei on 2016/8/31.
@@ -8,27 +9,32 @@ import java.util.ArrayList;
 
 public class Fenjie {
 
-  public static ArrayList<Integer> fenjie(int num) {
+  public static List<Integer> fenjie(int num) {
     int init = num;
-    ArrayList<Integer> list = new ArrayList<Integer>();
-    for (int i = 2; i <= num; i++) {
-      if (num % i == 0) {
-        num = num / i;
-        list.add(i);
+    List<Integer> list = new ArrayList<Integer>();
+    if (num == 1) {
+      list.add(1);
+      return list;
+    } else {
+      for (int i = 2; i <= num; i++) {
+        if (num % i == 0) {
+          num = num / i;
+          list.add(i);
+        }
       }
+      int count = 1;
+      for (Integer integer : list) {
+        count *= integer;
+      }
+      if (init / count != 1) {
+        list.add(init/count);
+      }
+      return list;
     }
-    int count = 1;
-    for (Integer integer : list) {
-      count *= integer;
-    }
-    if (init / count != 1) {
-      list.add(init/count);
-    }
-    return list;
   }
 
   public static void main(String[] args) {
-    ArrayList<Integer> list = Fenjie.fenjie(15);
+    List<Integer> list = Fenjie.fenjie(12);
     for (Integer integer : list) {
       System.out.println(integer);
     }
