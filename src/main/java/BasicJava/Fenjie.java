d9c5d3672c291b46e8edc6d94954a17ffa5191aa
zhangminglei <zml13856086071@163.com>
2016-08-31 20:57:52 +0800

    refine code

--- src/main/java/BasicJava/Fenjie.java
+++ src/main/java/BasicJava/Fenjie.java
@@ -10,31 +10,20 @@ import java.util.List;
 public class Fenjie {
 
   public static List<Integer> fenjie(int num) {
-    int init = num;
     List<Integer> list = new ArrayList<Integer>();
-    if (num == 1) {
-      list.add(1);
-      return list;
-    } else {
-      for (int i = 2; i <= num; i++) {
+      for (int i = 2; i <= num;) {
         if (num % i == 0) {
           num = num / i;
           list.add(i);
+        } else {
+          i++;
         }
       }
-      int count = 1;
-      for (Integer integer : list) {
-        count *= integer;
-      }
-      if (init / count != 1) {
-        list.add(init/count);
-      }
       return list;
-    }
   }
 
   public static void main(String[] args) {
-    List<Integer> list = Fenjie.fenjie(12);
+    List<Integer> list = Fenjie.fenjie(1);
     for (Integer integer : list) {
       System.out.println(integer);
     }
