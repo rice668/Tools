65cf9810ed8d7b12fb5f5b2009ecc59b50bf508a
zhangminglei <zml13856086071@163.com>
2016-09-05 16:34:18 +0800

    fenjie by Recur

--- src/main/java/BasicAlgorithms/HuaWei/Fenjie.java
+++ src/main/java/BasicAlgorithms/HuaWei/Fenjie.java
@@ -9,25 +9,41 @@ import java.util.Scanner;
  */
 
 public class Fenjie {
+  List<Integer> list = new ArrayList<Integer>();
 
   public static List<Integer> fenjie(int num) {
     List<Integer> list = new ArrayList<Integer>();
-      for (int i = 2; i <= num;) {
-        if (num % i == 0) {
-          num = num / i;
-          list.add(i);
-        } else {
-          i++;
-        }
+    for (int i = 2; i <= num; ) {
+      if (num % i == 0) {
+        num = num / i;
+        list.add(i);
+      } else {
+        i++;
       }
-      return list;
+    }
+    return list;
   }
+
+  public List<Integer> fenjieByRecur(int num) {
+    for (int i = 2; i <= num; ) {
+      if (num % i == 0) {
+        list.add(i);
+        num = num / i;
+        fenjieByRecur(num);
+        break;
+      } else {
+        i++;
+      }
+    }
+    return list;
+  }
+
 
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
     while (scanner.hasNext()) {
       int temp = scanner.nextInt();
-      List<Integer> list = Fenjie.fenjie(temp);
+      List<Integer> list = new Fenjie().fenjieByRecur(temp);
       for (Integer integer : list) {
         System.out.print(integer + " ");
       }
