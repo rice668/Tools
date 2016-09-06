a8508664702f4e3d1503daaa048729a4c8b68121
zhangminglei <zml13856086071@163.com>
2016-09-06 23:11:02 +0800

    add comment

--- src/main/java/BasicJava/ThreadPractice/NameList.java
+++ src/main/java/BasicJava/ThreadPractice/NameList.java
@@ -5,6 +5,16 @@ import java.util.List;
 
 /**
  * http://www.nowcoder.com/questionTerminal/25deb8d21e7d442e86c90302d6e03133
+ *
+ * G答案的执行顺序可以是：
+ 线程一：A、B、C
+ 线程二：A
+ 线程一：printAll()，输出A B C A
+ 线程二：B、C
+ 线程二：printAll()，输出A B C A B C
+ 故总得输出为  A B C A A B C A B C
+ *
+ * 不可能连续出现3个A
  */
 public class NameList {
   private List names = new ArrayList();
