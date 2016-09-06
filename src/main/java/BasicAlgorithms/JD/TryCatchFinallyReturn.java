803e394e03f11fbf82b635edabdee0188f9c3036
zhangminglei <zml13856086071@163.com>
2016-09-06 18:13:18 +0800

    try catch finally = > return issue

---
+++ src/main/java/BasicAlgorithms/JD/TryCatchFinallyReturn.java
@@ -0,0 +1,23 @@
+package BasicAlgorithms.JD;
+
+/**
+ * Created by zhangminglei on 2016/9/6.
+ */
+public class TryCatchFinallyReturn {
+
+  static int test() {
+    try {
+      // ... do
+      System.out.println("hello");
+      return 1;
+    } catch (Exception e) {
+      return 2;
+    } finally {
+      return 3;
+    }
+  }
+
+  public static void main(String[] args) {
+    System.out.println(test());
+  }
+}
